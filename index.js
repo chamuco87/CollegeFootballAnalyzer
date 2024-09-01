c72c37b4b7d2be9ed80a9203d1ac2832434389f5
Jose Carbajal <jose@carbajalsalinas.com>
2024-09-01 03:17:44 -0600

    Entire data set

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const { Console } = require('console');
             //await getSchedulePerYearDetails();
             //await getGamesPerYearDetails();
 
-            // var years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010];
+            // var years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005];
             // for (let index = 0; index < years.length; index++) {
             //     const yearTo = years[index];
             //     await prepareData(yearTo);
@@ -36,7 +36,7 @@ const { Console } = require('console');
             //     await generateAverages(yearTo);
             // }
 
-            //var years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010];
+            // var years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005];
             // for (let index = 0; index < years.length; index++) {
             //     const yearTo = years[index];
             //     var toBeEvaluated = false;
@@ -1338,7 +1338,12 @@ const { Console } = require('console');
                                                     var schedule_name = schedule.date_game.replace(" ","_").replace(" ","_").replace(",","");
                                                     var isProcessed = false;
                                                     try{
-                                                        isProcessed = await load(schedule_name + "_scoring", year.year_id+"/"+"Conferences"+"/"+school_name +"/Games") ? true : false;
+                                                        try{
+                                                            isProcessed = await load(schedule_name + "_team_stats", year.year_id+"/"+"Conferences"+"/"+school_name +"/Games") ? true : false;
+                                                        }
+                                                        catch{
+                                                            isProcessed = await load(schedule_name + "_results", year.year_id+"/"+"Conferences"+"/"+school_name +"/Games") ? true : false;
+                                                        }
                                                     }
                                                     catch(Ex){
                                                         if(schedule.date_gameLink){
